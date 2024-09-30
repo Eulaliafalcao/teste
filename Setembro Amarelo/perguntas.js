a9710363e57ca2bb39bbb8ddd838cc66878dd5f1
Eulaliafalcao <meulaliadeoliveirafalcao@gmail.com>
2024-09-30 14:29:39 -0300

    Add files via upload

---
+++ Setembro Amarelo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Animais',
+    'Qual o animal terrestre mais rápido do mundo?',
+    'Guepardo'
+)
+
+criaCartao(
+    'Flores',
+    'Qual a flor mais venenosa?',
+    'A espirradeira'
+)
+
+criaCartao(
+    'Arte',
+    'Qual foi a arte mais famosa?',
+    'A Mona Lisa de Leonardo da Vinci'
+)
+
+criaCartao(
+    'Música',
+    'Qual foi a música brasileira mais tocada no mundo?',
+    'Aquarela do Brasil'
+)
